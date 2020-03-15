9b7d99609653420d39cacf44092151a645441802
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-03-15 12:09:22 -0500

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/** @type Partial<import("prettier").RequiredOptions> */
+const prettierConfig = {
+    arrowParens: "always",
+    bracketSpacing: true,
+    endOfLine: "lf",
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 79,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "all",
+    useTabs: false,
+};
+
+module.exports = prettierConfig;
