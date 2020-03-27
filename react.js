83674b0eea9ba5840a05422b897097d3072d42b3
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-03-27 12:02:11 -0500

    Move all plugins to peer dependencies

--- react.js
+++ react.js
@@ -9,10 +9,15 @@ if (!ESLINT_PATH_BABELCONFIG || typeof ESLINT_PATH_BABELCONFIG !== "string") {
 
 /** @type {import("eslint").Linter.Config} */
 const reactEslintConfig = {
+    extends: [
+        "./index.js",
+        "plugin:react/recommended",
+        "prettier/babel",
+        "prettier/react",
+    ],
     env: {
         "shared-node-browser": true,
     },
-    parser: "@typescript-eslint/parser",
     parserOptions: {
         babelOptions: {
             configFile: ESLINT_PATH_BABELCONFIG,
@@ -24,19 +29,13 @@ const reactEslintConfig = {
     plugins: [
         "@typescript-eslint",
         "react",
-        "eslint-plugin-import-order-alphabetical",
-        "jsdoc",
-    ],
-    extends: [
-        "./index.js",
-        "plugin:react/recommended",
-        "prettier/babel",
-        "prettier/react",
     ],
     rules: {
-        // React
-        "react/prop-types": "off", // TypeScript handles props' types at build time, much better than runtime.
-        "react/display-name": "off", // Babel plugin now injects display name
+        // -- React -- \\
+        // TypeScript handles props' types at build time, instead of runtime
+        "react/prop-types": "off",
+        // Babel plugin now injects display name
+        "react/display-name": "off",
     },
     settings: {
         react: {
