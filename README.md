b40355a0e8f7c9ebf8fda8d9e3d2054da6123bc1
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-05-17 11:16:25 -0500

    Remove all non react/tsx eslint config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# eslint-config
-My simple eslint config for TS + JS + Prettier + [React]
+# eslint-config-react
+
+My simple eslint config for TS + JS + Prettier + React
+
+Should be used in conjunction with my base ESLint config here: [https://github.com/JacobFischer/eslint-config]
--- index.js
+++ index.js
@@ -1,80 +1,56 @@
 // @ts-check
 /* eslint-env node */
 
-const { dirname } = require("path");
-const { ESLINT_PATH_TSCONFIG } = process.env;
+const { ESLINT_PATH_BABELCONFIG } = process.env;
 
-if (!ESLINT_PATH_TSCONFIG || typeof ESLINT_PATH_TSCONFIG !== "string") {
-    throw new Error("Cannot use this eslint config file with process.env.ESLINT_PATH_TSCONFIG set");
+if (!ESLINT_PATH_BABELCONFIG || typeof ESLINT_PATH_BABELCONFIG !== "string") {
+    throw new Error("Cannot use this eslint config file with process.env.ESLINT_PATH_BABELCONFIG set");
 }
 
-// const ENABLED_ERROR = "error";
-const ENABLED_WARNING = "warn";
-
 /** @type {import("eslint").Linter.Config} */
-const baseEslintConfig = {
+const reactEslintConfig = {
+    extends: [
+        "plugin:react/recommended",
+        "prettier/babel",
+        "prettier/react",
+    ],
     env: {
         es6: true,
+        "shared-node-browser": true,
     },
-    parser: "@typescript-eslint/parser",
     parserOptions: {
-        ecmaVersion: 2020,
-        project: ESLINT_PATH_TSCONFIG,
-        sourceType: "module",
+        babelOptions: {
+            configFile: ESLINT_PATH_BABELCONFIG,
+        },
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
     plugins: [
         "@typescript-eslint",
-        "eslint-plugin-import-order-alphabetical",
-        "jsdoc",
-        "import",
-    ],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        "plugin:jsdoc/recommended",
-        "plugin:prettier/recommended",
-        "prettier/@typescript-eslint",
+        "react",
     ],
     rules: {
-        // TypeScript
-        "@typescript-eslint/explicit-function-return-type": "off", // For now does not allow enough control over arrow functions, always requiring return types even on simple reducers and such.
-
-        // JSDoc
-        "jsdoc/no-types": 1, // all jsdoc type rules disabled because TypeScript
-        "jsdoc/require-param-type": "off",
-        "jsdoc/require-returns-type": "off",
-        "jsdoc/no-undefined-types": "off",
-        "jsdoc/check-indentation": [ENABLED_WARNING],
-        "jsdoc/require-description": [ENABLED_WARNING],
-        "jsdoc/require-description-complete-sentence": [ENABLED_WARNING],
-        "jsdoc/require-hyphen-before-param-description": [
-            ENABLED_WARNING,
-            "always",
-        ],
+        // -- React -- \\
+        // TypeScript handles props' types at build time, instead of runtime
+        "react/prop-types": "off",
+        // Babel plugin now injects display name
+        "react/display-name": "off",
     },
     settings: {
-        "import/extensions": [".js", ".ts"],
+        react: {
+            version: "detect",
+        },
+        "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
         "import/resolver": {
             node: {
-                extensions: [".js", ".ts"],
-            },
-            typescript: {
-                alwaysTryTypes: true,
-                directory: dirname(ESLINT_PATH_TSCONFIG),
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
             },
         },
         "import/parsers": {
-            "@typescript-eslint/parser": [".ts"],
-        },
-        jsdoc: {
-            mode: "typescript",
+            "@typescript-eslint/parser": [".ts", ".tsx"],
         },
     },
 };
 
-module.exports = baseEslintConfig;
+module.exports = reactEslintConfig;
